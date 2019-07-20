e50c1b047547f4403fc02968c4768346a03ec63d
Igor Costa <igor.vc30@gmail.com>
2019-07-20 11:07:58 -0300

    chore: criação de mock server e integração da lista de produtos com os dados do server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,70 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://utennis.com.br/media/catalog/product/cache/1/image/512x512/9df78eab33525d08d6e5fb8d27136e95/b/7/b700n-7743-tenis-asics-gel-volley-elite-ff-mt-verde-e-azul.jpg"
-          alt="Melhor tenis de volei"
-        />
-        <strong>Tênis Asics Gel-Volley Elite FF mt</strong>
-        <span>R$350,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://utennis.com.br/media/catalog/product/cache/1/image/512x512/9df78eab33525d08d6e5fb8d27136e95/b/7/b700n-7743-tenis-asics-gel-volley-elite-ff-mt-verde-e-azul.jpg"
-          alt="Melhor tenis de volei"
-        />
-        <strong>Tênis Asics Gel-Volley Elite FF mt</strong>
-        <span>R$350,00</span>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://utennis.com.br/media/catalog/product/cache/1/image/512x512/9df78eab33525d08d6e5fb8d27136e95/b/7/b700n-7743-tenis-asics-gel-volley-elite-ff-mt-verde-e-azul.jpg"
-          alt="Melhor tenis de volei"
-        />
-        <strong>Tênis Asics Gel-Volley Elite FF mt</strong>
-        <span>R$350,00</span>
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://utennis.com.br/media/catalog/product/cache/1/image/512x512/9df78eab33525d08d6e5fb8d27136e95/b/7/b700n-7743-tenis-asics-gel-volley-elite-ff-mt-verde-e-azul.jpg"
-          alt="Melhor tenis de volei"
-        />
-        <strong>Tênis Asics Gel-Volley Elite FF mt</strong>
-        <span>R$350,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
